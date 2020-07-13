b45c28865c05a6a6d8f29e3e9269eb6035600d2a
Elliott Stein <estein1988@gmail.com>
2020-07-13 21:35:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,24 +5,23 @@ def key_for_min_value(name_hash)
   lowest_key = nil 
   lowest_value = nil 
   name_hash.each do |k, v|
-    if lowest_value == nil || 
-  name_hash.each do |name|
-    if name.length < name.length
-      name
-    end
-  end
-shortest
-end
-
-
-def key_for_min_value(hash)
-  lowest_key = nil
-  lowest_value = nil
-  hash.each do |k, v|
     if lowest_value == nil || v < lowest_value
-      lowest_value = v
-      lowest_key = k
-    end
-  end
+      lowest_value = v 
+      lowest_key = k 
+    end 
+  end 
   lowest_key
 end
+
+
+#def key_for_min_value(hash)
+ # lowest_key = nil
+  #lowest_value = nil
+  #hash.each do |k, v|
+   # if lowest_value == nil || v < lowest_value
+    #  lowest_value = v
+     # lowest_key = k
+   # end
+  #end
+  #lowest_key
+#end
