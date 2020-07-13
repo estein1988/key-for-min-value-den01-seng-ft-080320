756bdb5e977ad2e023b625c79ab1563c18c475c2
Elliott Stein <estein1988@gmail.com>
2020-07-13 21:18:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  short = ""
   name_hash.each do |name|
-    name.collect
+    if name < name
+      short = name
   end
   name
+end
+
+def select_winner(passengers)
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
+winner
 end
